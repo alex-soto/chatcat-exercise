c770f7a2c863918003abdcd70c632d3b4139e1e4
Alex Soto <as1694@scarletmail.rutgers.edu>
2016-12-03 18:03:43 -0500

    Begin mounting the express routes

--- app/index.js
+++ app/index.js
@@ -1,11 +1,5 @@
 'use strict';
-const router = require('express').Router();
-
-router.get('/', (req, res, next) => {
-	//res.sendFile(__dirname + '/views/login.htm');
-	res.render('login');
-});
 
 module.exports = {
-	router: router
+	router: require('./routes')()
 };
