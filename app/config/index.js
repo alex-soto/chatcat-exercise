b03de2517f89d6cdded64eda1de27c1beef5e129
Alex Soto <as1694@scarletmail.rutgers.edu>
2016-12-05 10:18:38 -0500

    Address mongolabs connection timeout issue. workaround: connect to local mongodb instance.

--- app/config/index.js
+++ app/config/index.js
@@ -2,6 +2,8 @@
 
 if (process.env.NODE_ENV === 'production'){
 	// Offer production stage environment variables
+	// mLab URI:
+	// "mongodb://chatcatuser:P4SSW0rd0865@ds119578.mlab.com:19578/mychatcatdb-as"
 	module.exports = {
 		host: process.env.host || "",
 		dbURI: process.env.dbURI,
