ff620f1e7c61eb07c1ed2b3faf3d6170f94eb1e1
Alex Soto <as1694@scarletmail.rutgers.edu>
2016-12-06 12:10:14 -0500

    Implement twitter authentication strategy

--- app/auth/index.js
+++ app/auth/index.js
@@ -3,6 +3,7 @@ const passport = require('passport');
 const config = require('../config');
 const util = require('../utilities');
 const FacebookStrategy = require('passport-facebook').Strategy;
+const TwitterStrategy = require('passport-twitter').Strategy;
 
 module.exports = () => {
 	passport.serializeUser((user, done) => {
@@ -37,4 +38,5 @@ module.exports = () => {
 	};
 	
 	passport.use(new FacebookStrategy(config.fb, authProcessor));
+	passport.use(new TwitterStrategy(config.twitter, authProcessor));
 };
--- app/session/index.js
+++ app/session/index.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'production'){
 	// Initialize session with settings for production
 	module.exports = session({
 		secret: config.sessionSecret,
-		resave: false,
+		resave: true,
 		saveUninitialized: false,
 		store: new MongoStore({
 			mongooseConnection: db.Mongoose.connection
