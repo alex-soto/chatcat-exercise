c29a79dbd8b8085782d84c7aca84c5ced9cd1404
AlexSoto <alexander.soto@rutgers.edu>
2016-12-08 20:47:26 +0000

    Modify socket module and rooms page to send, receive, and render chatroom data

--- app/socket/index.js
+++ app/socket/index.js
@@ -1,7 +1,22 @@
 'use strict';
 module.exports = (io, app) => {
 	let allrooms = app.locals.chatroom;
+	
+	allrooms.push({
+	    room: 'Good Food',
+	    roomID: '0001',
+	    users: []
+	});
+	
+	allrooms.push({
+	    room: 'Cloud computing',
+	    roomID: '0002',
+	    users: []
+	});
+	
 	io.of('/roomslist').on('connection', socket => {
-	    console.log("Socket.io connected to client!");
+	    socket.on('getChatRooms', () => {
+	        socket.emit('chatRoomsList', JSON.stringify(allrooms));
+	    });
 	});
 }
