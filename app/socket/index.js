ee465c04c017592424b193c846a7a371b025ad89
AlexSoto <alexander.soto@rutgers.edu>
2016-12-09 01:00:15 +0000

    Remove example chatrooms

--- app/socket/index.js
+++ app/socket/index.js
@@ -2,18 +2,6 @@
 module.exports = (io, app) => {
 	let allrooms = app.locals.chatroom;
 	
-	allrooms.push({
-	    room: 'Good Food',
-	    roomID: '0001',
-	    users: []
-	});
-	
-	allrooms.push({
-	    room: 'Cloud computing',
-	    roomID: '0002',
-	    users: []
-	});
-	
 	io.of('/roomslist').on('connection', socket => {
 	    socket.on('getChatRooms', () => {
 	        socket.emit('chatRoomsList', JSON.stringify(allrooms));
